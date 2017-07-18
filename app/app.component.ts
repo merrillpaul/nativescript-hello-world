2c7ae8c54538085095b2e15bef2ec9e430f6ca32
merrillpaul <merrill.paul@pearson.com>
2017-07-18 15:55:34 -0500

    Added animation

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,9 +3,22 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "my-app",
   template: `
-    <ActionBar [title]="message"></ActionBar>
+    <ActionBar [title]="message" class="action-bar"></ActionBar>
     <!-- Your UI components go here -->
-  `
+    <Image src="~/images/apple.jpg"></Image>
+  `,
+  styles: [
+    `
+    @keyframes spin {
+      from { transform: rotate(0); } to { transform: rotate(360); }
+    }
+    Image {
+      animation-name: spin; animation-duration: 3s;
+      animation-iteration-count: infinite;
+      animation-timing-function: linear;
+    }
+    `
+  ]
 })
 export class AppComponent implements OnInit {
   message: string = 'Tester App'
